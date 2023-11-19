2ac871b0ce7c329fdabc0f1024c7aada815aaed1
Valeriy Kharkov <iam@vkharkov.ru>
2023-11-19 13:36:16 +0300

    docker updated, swagger generator

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                 */
-                'api' => 'api/doc',
+                'api' => '/docs',
             ],
             'paths' => [
                 /*
